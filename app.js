f9525beb4bab16c0e7e0c846f5485c45812d5489
manish-chi <chitre.manish@gmail.com>
2024-08-21 13:32:54 +0530

    added cros settings

--- app.js
+++ app.js
@@ -24,12 +24,13 @@ if (process.env.NODE_ENV == "development") {
 
 app.use(express.json());
 
-app.use(cors({
-  origin: '*',
-  credentials:true,            //access-control-allow-credentials:true
-  optionSuccessStatus:200,
-}));
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
+app.use(cors());
 
 app.use('/api/v1/users',userRouter);
 app.use("/api/v1/restaurants", restaurantRouter);
