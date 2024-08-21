b6ca61bff22df40b414e7c6ed48248535d32424d
manish-chi <chitre.manish@gmail.com>
2024-08-21 13:55:08 +0530

    removed  cros settings

--- app.js
+++ app.js
@@ -24,12 +24,6 @@ if (process.env.NODE_ENV == "development") {
 
 app.use(express.json());
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
-
 app.use(cors());
 
 app.use('/api/v1/users',userRouter);
