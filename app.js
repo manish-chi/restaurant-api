e9a6cbe1d8ad5c4b36ecb2dd9bdbe711e9e0aa45
manish-chi <chitre.manish@gmail.com>
2024-08-23 10:33:23 +0530

    added routes to show all tasks

--- app.js
+++ app.js
@@ -46,6 +46,21 @@ app.use(express.json());
 //   next();
 // });
 
+router.get('/', function(req, res, next) {
+  Task.find()
+    .then((tasks) => {      
+      const currentTasks = tasks.filter(task => !task.completed);
+      const completedTasks = tasks.filter(task => task.completed === true);
+
+      console.log(`Total tasks: ${tasks.length}   Current tasks: ${currentTasks.length}    Completed tasks:  ${completedTasks.length}`)
+      res.render('index', { currentTasks: currentTasks, completedTasks: completedTasks });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.send('Sorry! Something went wrong.');
+    });
+});
+
 app.use('/config',authRouter);
 app.use('/api/v1/users',userRouter);
 app.use("/api/v1/restaurants", restaurantRouter);
