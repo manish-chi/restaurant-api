2a25f23a73cd172448d57a1a9e9b14ba1ea64980
manish-chi <chitre.manish@gmail.com>
2025-05-02 11:21:38 +0530

    added demo user name in directline request

--- controllers/authController.js
+++ controllers/authController.js
@@ -203,6 +203,7 @@ const generateDirectLineToken = catchAsync(async (req, res, next) => {
           "Content-Type": "application/json",
         },
         body: JSON.stringify({
+          //demo user.
           user: { id: userId, name: "manish chitre" },
           trustedOrigins: ["http://localhost:8000", "http://localhost:3000","https://purple-pebble-09b781300.6.azurestaticapps.net/"],
         }),
